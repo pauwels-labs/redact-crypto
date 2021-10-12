7555ed4150d1079307ea34e9e443ea7693e1fd8e
Alexandre Pauwels <8890201+ajpauwels@users.noreply.github.com>
2021-10-12 14:26:23 +0100

    fix: serialize/deserialize nonces to base64
    - Nonces never had custom serialization/deserialization functions to
      convert the raw binary bytes into base64, leading to a custom representation
      in mongo
    - These functions have been added and nonces will appear as base64-strings
      when serialized

--- src/nonce/sodiumoxide.rs
+++ src/nonce/sodiumoxide.rs
@@ -1,4 +1,4 @@
-use serde::{Deserialize, Serialize};
+use serde::{de, Deserialize, Deserializer, Serialize, Serializer};
 use sodiumoxide::crypto::{
     box_::{self, Nonce as ExternalAsymmetricNonce, NONCEBYTES as EXTERNALASYMMETRICNONCEBYTES},
     secretbox::{self, Nonce as ExternalSymmetricNonce, NONCEBYTES as EXTERNALSYMMETRICNONCEBYTES},
@@ -6,7 +6,37 @@ use sodiumoxide::crypto::{
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct SodiumOxideSymmetricNonce {
+    #[serde(
+        serialize_with = "symmetric_nonce_serialize",
+        deserialize_with = "symmetric_nonce_deserialize"
+    )]
     pub nonce: ExternalSymmetricNonce,
+}
+
+/// Custom serialization function base64-encodes the bytes before storage
+fn symmetric_nonce_serialize<S>(nonce: &ExternalSymmetricNonce, s: S) -> Result<S::Ok, S::Error>
+where
+    S: Serializer,
+{
+    let b64_encoded = base64::encode(nonce.as_ref());
+    s.serialize_some(&Some(b64_encoded))
+}
+
+/// Custom deserialization function base64-decodes the bytes before passing them back
+fn symmetric_nonce_deserialize<'de, D>(deserializer: D) -> Result<ExternalSymmetricNonce, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let b64_encoded: String = de::Deserialize::deserialize(deserializer)?;
+    let decoded = base64::decode(b64_encoded).map_err(de::Error::custom)?;
+    let nonce = ExternalSymmetricNonce::from_slice(decoded.as_ref());
+    match nonce {
+        Some(n) => Ok(n),
+        None => Err(de::Error::custom(format!(
+            "deserialized nonce was {} bytes long, expected 24 bytes",
+            decoded.len()
+        ))),
+    }
 }
 
 impl SodiumOxideSymmetricNonce {
@@ -27,7 +57,39 @@ impl SodiumOxideSymmetricNonce {
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct SodiumOxideAsymmetricNonce {
+    #[serde(
+        serialize_with = "asymmetric_nonce_serialize",
+        deserialize_with = "asymmetric_nonce_deserialize"
+    )]
     pub nonce: ExternalAsymmetricNonce,
+}
+
+/// Custom serialization function base64-encodes the bytes before storage
+fn asymmetric_nonce_serialize<S>(nonce: &ExternalAsymmetricNonce, s: S) -> Result<S::Ok, S::Error>
+where
+    S: Serializer,
+{
+    let b64_encoded = base64::encode(nonce.as_ref());
+    s.serialize_some(&Some(b64_encoded))
+}
+
+/// Custom deserialization function base64-decodes the bytes before passing them back
+fn asymmetric_nonce_deserialize<'de, D>(
+    deserializer: D,
+) -> Result<ExternalAsymmetricNonce, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let b64_encoded: String = de::Deserialize::deserialize(deserializer)?;
+    let decoded = base64::decode(b64_encoded).map_err(de::Error::custom)?;
+    let nonce = ExternalAsymmetricNonce::from_slice(decoded.as_ref());
+    match nonce {
+        Some(n) => Ok(n),
+        None => Err(de::Error::custom(format!(
+            "deserialized nonce was {} bytes long, expected 24 bytes",
+            decoded.len()
+        ))),
+    }
 }
 
 impl SodiumOxideAsymmetricNonce {
